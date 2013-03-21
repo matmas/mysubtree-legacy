cfea6b6d1f625687597acd4e2ae43f0de0767fad
Martin Riesz <riesz.martin@gmail.com>
2013-03-21 23:59:49 +0100

    refactored assetwatch a tiny bit

--- src/lib/assetwatch/assetobserver.py
+++ src/lib/assetwatch/assetobserver.py
@@ -8,9 +8,9 @@ from registers.imageregister import ImageRegister
 from registers.scriptregister import ScriptRegister
 
 class EventHandler(FileSystemEventHandler):
-    def __init__(self, register):
+    def __init__(self, registers):
         FileSystemEventHandler.__init__(self)
-        self.register = register
+        self.registers = registers
 
     def on_any_event(self, event):
         file = event.src_path
@@ -18,31 +18,31 @@ class EventHandler(FileSystemEventHandler):
             print "[debug] watchdog %s: %s -> %s" % (type(event).__name__, event.src_path, event.dest_path)
         else:
             print "[debug] watchdog %s: %s" % (type(event).__name__, event.src_path)
-        if extension(file) in self.register.recognized_extensions():
-            if type(event) == FileCreatedEvent:
-                self.register.new_file_detected(file)
-            elif type(event) == FileDeletedEvent:
-                self.register.file_deleted(file)
-            elif type(event) == FileModifiedEvent:
-                self.register.file_modified(file)
-        if type(event) == FileMovedEvent:
-            if extension(event.src_path) in self.register.recognized_extensions():
-                self.register.file_deleted(event.src_path)
-            if extension(event.dest_path) in self.register.recognized_extensions():
-                self.register.new_file_detected(event.dest_path)
+            
+        for register in self.registers:
+            if extension(file) in register.recognized_extensions():
+                if type(event) == FileCreatedEvent:
+                    register.new_file_detected(file)
+                elif type(event) == FileDeletedEvent:
+                    register.file_deleted(file)
+                elif type(event) == FileModifiedEvent:
+                    register.file_modified(file)
+            if type(event) == FileMovedEvent:
+                if extension(event.src_path) in register.recognized_extensions():
+                    register.file_deleted(event.src_path)
+                if extension(event.dest_path) in register.recognized_extensions():
+                    register.new_file_detected(event.dest_path)
 
 def assetobserver(assets_dir, public_dir, http_path, environment):
     observer = Observer()
-    
     imageregister = ImageRegister(assets_dir, public_dir, http_path, environment)
-    observer.schedule(EventHandler(imageregister), path=assets_dir, recursive=True)
-    
     sassregister = SassRegister(assets_dir, public_dir, http_path, environment, imageregister)
-    observer.schedule(EventHandler(sassregister), path=assets_dir, recursive=True)
-    
     scriptregister = ScriptRegister(assets_dir, public_dir, http_path, environment)
-    observer.schedule(EventHandler(scriptregister), path=assets_dir, recursive=True)
-    
+    observer.schedule(EventHandler([
+        imageregister,
+        sassregister,
+        scriptregister,
+    ]), path=assets_dir, recursive=True)
     observer.start()
     try:
         while True:
