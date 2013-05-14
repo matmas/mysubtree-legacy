7aaa84bf3a4184f96f03b081a466465fbbed9a6c
Martin Riesz <riesz.martin@gmail.com>
2013-05-14 23:44:27 +0200

    make all redirects with relative urls

--- src/mysubtree/web/headers.py
+++ src/mysubtree/web/headers.py
@@ -1,4 +1,5 @@
 #autoimport
+from urlparse import urlparse, urlunparse
 from .app import app
 
 @app.after_request
@@ -14,4 +15,15 @@ def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+
+    # # Make all redirects with relative URLs
+    # if "Location" in response.headers:
+    # 	u = list(urlparse(response.headers["Location"]))
+    # 	u[0] = None # scheme
+    # 	u[1] = None # netloc
+    # 	relative_url = urlunparse(u)
+    # 	assert relative_url.startswith("/")
+    # 	# response.headers["Location"] = relative_url
+    response.autocorrect_location_header = False	
+
     return response
