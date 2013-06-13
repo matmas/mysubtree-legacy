394a72485cf41bf2ae3d266533bfcbeed2acb270
Martin Riesz <riesz.martin@gmail.com>
2013-06-13 11:06:24 +0200

    terms of use update

--- src/mysubtree/web/components/int/routes/account/terms/terms.py
+++ src/mysubtree/web/components/int/routes/account/terms/terms.py
@@ -1,4 +1,5 @@
 #autoimport
+import os.path, time
 from mysubtree.web.app import app
 from mysubtree.web.templating import render_template
 from mysubtree.web.babel import set_locale
@@ -6,12 +7,14 @@ from mysubtree.web.babel import set_locale
 @app.route("/<lang>/terms")
 def terms(lang):
     set_locale(lang)
+    template = "terms.html"
+    template_file = os.path.join(os.path.dirname(__file__), template)
     variables = {
         "DOMAIN": "mysubtree.org",
-        "COMPANY_NAME": "Mysubtree",
+        "COMPANY_NAME": "Matmas",
         "STATE": "United States",
         "COUNTRY": "California",
-        
+        "last_updated": time.strftime("%b %d, %Y", time.gmtime(os.path.getmtime(template_file))),
     }
-    return render_template("terms.html", lang=lang, **variables)
+    return render_template(template, lang=lang, **variables)
 
