e4494eeb56967521e1e905b7de7083aa94c1420f
Martin Riesz <riesz.martin@gmail.com>
2013-06-20 22:58:45 +0200

    show creation date without time to users of nodes

--- src/mysubtree/web/components/int/node/inside/timesince/timesince.py
+++ src/mysubtree/web/components/int/node/inside/timesince/timesince.py
@@ -1,4 +1,4 @@
-from flaskext.babel import format_datetime, gettext as _
+from flaskext.babel import format_date, gettext as _
 from lib.html import Html
 from mysubtree.backend.models.node.types.all import get_model
 from mysubtree.web.templatefilters import timesince as timesince_filter, activity_level
@@ -13,7 +13,7 @@ def timesince(node):
         ],
         title="%(created)s %(exactly)s" % {
             "created": _("created"),
-            "exactly": format_datetime(node.created)},
+            "exactly": format_date(node.created)},
     ):
         html.text(timesince_filter(node.created))
         html.text(" ")
