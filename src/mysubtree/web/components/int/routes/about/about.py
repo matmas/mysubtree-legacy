7d8a68f5ed9c024c254b5f800d8873f35f1e56f8
Martin Riesz <riesz.martin@gmail.com>
2013-08-30 01:11:17 +0200

    added preliminary about page at /en/about

---
+++ src/mysubtree/web/components/int/routes/about/about.py
@@ -0,0 +1,10 @@
+#autoimport
+from mysubtree.web.app import app
+from mysubtree.web.templating import render_template
+from mysubtree.web.babel import set_locale
+
+@app.route("/<lang>/about")
+def about(lang):
+    set_locale(lang)
+    return render_template("about.html")
+
